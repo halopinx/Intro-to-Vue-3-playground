78f23d5e5fa7703bdea23317b8661be26b6c46af
Errah Fuschia Gure <errah@annix.com.au>
2022-10-29 15:23:59 +0800

    style and class binding to button add to cart and color variants

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
             product: 'Socks',
             inventory: 8,
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
