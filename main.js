64319b53adf5dee1d43c2fbe82bf304684aa6d12
Errah Fuschia Gure <errah@annix.com.au>
2022-10-30 17:57:45 +0800

    create componen product display and add shipping base on premium props

--- main.js
+++ main.js
@@ -2,39 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            // image: './assets/images/socks_blue.jpg',
-            // inStock: false,
-            selectedVariant: 0,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 8 },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ]
+            premium: true
         }
     },
     methods: {
-        addToCart(){
-            this.cart += 1;
-        },
-        // updateImage(img){
-        //     this.image = img
-        // },
-        updateVariant(index){
-            this.selectedVariant = index;
-            console.log(index)
-        }
-    },
-    computed: {
-        title() {
-            return `${this.product} - ${this.brand}`
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        }
     }
 })
